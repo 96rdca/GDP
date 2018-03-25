5192494c3087e6d8a6a692ccb0ff3502015e6836
Richard C <32852741+96rdca@users.noreply.github.com>
2018-03-25 16:08:22 -0400

    Add project files.

---
+++ GestionProyectos_Final/Controllers/DocumentsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionProyectos_Final.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        public ActionResult ByProjects()
+        {
+            return View();
+        }
+    }
+}
---
+++ GestionProyectos_Final/Controllers/ProjectsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionProyectos_Final.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: Prjects
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Add Prjects
+        public ActionResult AddProject()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
